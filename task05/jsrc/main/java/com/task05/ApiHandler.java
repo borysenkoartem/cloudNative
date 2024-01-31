c92eef847a11bbf429905338048a0908d7b3e386
Artem Borysenko <artem_borysenko@epam.com>
2024-01-31 22:11:00 +0100

    Task5

--- task05/jsrc/main/java/com/task05/ApiHandler.java
+++ task05/jsrc/main/java/com/task05/ApiHandler.java
@@ -8,8 +8,10 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.events.DynamoDbTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.LambdaSnapStart;
+import com.syndicate.deployment.model.ResourceType;
 
 import java.time.Instant;
 import java.util.HashMap;
@@ -22,6 +24,7 @@ import java.util.UUID;
         snapStart = LambdaSnapStart.PublishedVersions
 )
 @DynamoDbTriggerEventSource(targetTable = "Events", batchSize = 1)
+@DependsOn(name = "Events", resourceType = ResourceType.DYNAMODB_TABLE)
 
 public class ApiHandler implements RequestHandler<Map<String, Object>, Map<String, Object>> {
 
