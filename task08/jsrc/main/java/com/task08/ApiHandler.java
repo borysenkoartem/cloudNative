2a35dee9bfabb4d9d7d5c95d0a990cfbf803a981
Artem Borysenko <artem_borysenko@epam.com>
2024-02-09 18:10:57 +0100

    Task8

---
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -0,0 +1,47 @@
+package com.task08;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.epam.OpenMeteoAPIClient;
+import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
+import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
+
+@LambdaHandler(lambdaName = "api_handler",
+	roleName = "api_handler-role"
+)
+@LambdaLayer(
+		layerName = "sdk-layer",
+		libraries = {"lib/OpenMetio.jar"},
+		runtime = DeploymentRuntime.JAVA8,
+		artifactExtension = ArtifactExtension.ZIP
+)
+@LambdaUrlConfig(
+		authType = AuthType.NONE,
+		invokeMode = InvokeMode.BUFFERED
+)
+public class ApiHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+	private static final int SC_OK = 200;
+	private static final int SC_BAD_REQUEST = 400;
+	private final OpenMeteoAPIClient apiClient = new OpenMeteoAPIClient();
+
+	@Override
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+		try {
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_OK)
+					.withBody(apiClient.getWeatherForecast());
+		} catch (IllegalArgumentException exception) {
+			return new APIGatewayProxyResponseEvent()
+					.withStatusCode(SC_BAD_REQUEST)
+					.withBody("ERROR");
+		}
+	}
+}
