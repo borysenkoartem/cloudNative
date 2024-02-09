8262d5cc036ac85b6bee623faee26eaa6fa2b90c
Artem Borysenko <artem_borysenko@epam.com>
2024-02-09 22:52:41 +0100

    Task9

--- task09/jsrc/main/java/com/task09/Processor.java
+++ task09/jsrc/main/java/com/task09/Processor.java
@@ -7,7 +7,10 @@ import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.epam.OpenMeteoAPIClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.syndicate.deployment.annotations.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariable;
+import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.model.ArtifactExtension;
@@ -15,40 +18,57 @@ import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.TracingMode;
 import com.syndicate.deployment.model.lambda.url.AuthType;
 import com.syndicate.deployment.model.lambda.url.InvokeMode;
+import com.fasterxml.jackson.core.type.TypeReference;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 @LambdaHandler(lambdaName = "processor",
-		roleName = "processor-role",
-		layers = {"sdk-layer"},
-		tracingMode = TracingMode.Active
+        roleName = "processor-role",
+        layers = {"sdk-layer"},
+        tracingMode = TracingMode.Active
 )
 @LambdaLayer(
-		layerName = "sdk-layer",
-		libraries = {"lib/OpenMetio-1.0.jar"},
-		runtime = DeploymentRuntime.JAVA8,
-		artifactExtension = ArtifactExtension.ZIP
+        layerName = "sdk-layer",
+        libraries = {"lib/OpenMetio-1.0.jar"},
+        runtime = DeploymentRuntime.JAVA8,
+        artifactExtension = ArtifactExtension.ZIP
 )
 @LambdaUrlConfig(
-		authType = AuthType.NONE,
-		invokeMode = InvokeMode.BUFFERED
+        authType = AuthType.NONE,
+        invokeMode = InvokeMode.BUFFERED
 )
+@EnvironmentVariables(value = {
+        @EnvironmentVariable(key = "region", value = "${region}"),
+        @EnvironmentVariable(key = "target_table", value = "${target_table}")
+})
 public class Processor implements RequestHandler<Object, Map<String, Object>> {
 
-	private static final String WEATHER_TABLE = "cmtr-a655d43a-Weather-test";
-	private final OpenMeteoAPIClient apiClient = new OpenMeteoAPIClient();
+    private final OpenMeteoAPIClient apiClient = new OpenMeteoAPIClient();
+    private final DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
-	public Map<String, Object> handleRequest(Object request, Context context) {
-		DynamoDB dynamoDB = new DynamoDB(AmazonDynamoDBClientBuilder.defaultClient());
-		Table eventsTable = dynamoDB.getTable(WEATHER_TABLE);
-		Item item = new Item().withPrimaryKey("id", UUID.randomUUID().toString())
-				.with("forecast", apiClient.getWeatherForecast());
-		eventsTable.putItem(item);
-		Map<String, Object> responseMap = new HashMap<>();
-		responseMap.put("statusCode", 200);
-		responseMap.put("forecast", item.toJSONPretty());
-		return responseMap;
-	}
+    public Map<String, Object> handleRequest(Object request, Context context) {
+        try {
+            String tableName = System.getenv("target_table");
+            Table eventsTable = dynamoDB.getTable(tableName);
+            String rawJsonForecast = apiClient.getWeatherForecast();
+            TypeReference<HashMap<String, Object>> typeRef = new TypeReference<HashMap<String, Object>>() {
+            };
+
+            Object forecastMap = objectMapper.readValue(rawJsonForecast, typeRef);
+
+            Item item = new Item().withPrimaryKey("id", UUID.randomUUID().toString())
+                    .with("forecast", forecastMap);
+            eventsTable.putItem(item);
+            Map<String, Object> responseMap = new HashMap<>();
+            responseMap.put("statusCode", 200);
+            responseMap.put("body", item.toJSONPretty());
+            return responseMap;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
